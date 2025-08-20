4e61d1551d815166da453a4ed8d7a51e48723007
Chris Gallagher <chris@chrisgallagher.net>
2025-08-20 19:17:58 -0400

    Publish site to docs for GitHub Pages

---
+++ docs/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1755731814',resources: [
+      '/ChrisGallagher/assets/css/jekyll-theme-chirpy.css',
+      '/ChrisGallagher/',
+      
+        '/ChrisGallagher/categories/',
+      
+        '/ChrisGallagher/tags/',
+      
+        '/ChrisGallagher/archives/',
+      
+        '/ChrisGallagher/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
